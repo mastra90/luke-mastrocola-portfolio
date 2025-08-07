87ba3f6cbd23d2a72ce4299b44def5a88a8ac702
Luke Mastrocola <lukejmastrocola@gmail.com>
2025-08-07 23:53:35 +1000

    Script feature:
    - Better error handling in the script when Docker is not running. Gives user an option to run docker and continue

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,53 @@ REPO_URL="https://github.com/mastra90/luke-mastrocola-portfolio.git"
 PROJECT_DIR="luke-mastrocola-portfolio"
 PORT=5173
 
+# Docker check loop
+set +e
+
+LINES_TO_CLEAR=8
+
+while true; do
+    if [[ "$RETRY" == true ]]; then
+        for ((i = 0; i < LINES_TO_CLEAR; i++)); do
+            tput cuu1
+            tput el
+        done
+    fi
+
+    echo -e "🔍 Checking if Docker services are running...\n"
+
+    if docker info > /dev/null 2>&1; then
+        echo "✅ Docker is running!"
+        echo ""
+        break
+    else
+        sleep 1
+        echo "❌ Docker is not running."
+        echo ""
+        echo "🐳 Run Docker and press y to continue."
+        echo "🐳 Press any other key to cancel."
+        echo ""
+        read -p "🔁 Continue? (y): " input
+
+        if [[ "$input" == [Yy] ]]; then
+            RETRY=true
+            continue
+        else
+            echo -e "\n👋 Deployment cancelled"
+            exit 0
+        fi
+    fi
+done
+
+set -e
+echo ""
 echo "🚀 Deploying luke-mastrocola-portfolio..."
 
 PARENT_DIR=$(pwd)
 
 if [ -d "$PROJECT_DIR" ]; then
     echo "🗑️ Removing existing directory..."
+    echo ""
     chmod -R u+w "$PROJECT_DIR" 2>/dev/null || true
     rm -rf "$PROJECT_DIR" 2>/dev/null || sudo rm -rf "$PROJECT_DIR"
 fi
